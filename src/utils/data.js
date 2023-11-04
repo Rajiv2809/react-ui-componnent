18bfcd7a3e94f86e1c72c91039c5069eb15729e7
Rajiv2809 <121161360+Rajiv2809@users.noreply.github.com>
2023-11-04 16:37:15 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,25 @@
+
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Rajiv',
+        tag: 'rjiv.1',
+        imageUrl: 'https://i.pinimg.com/564x/b4/3a/89/b43a892e3f68c50a5b7ce996aa41a1af.jpg',
+      },
+      {
+        id: 2,
+        name: 'Syakila',
+        tag: 'syakila',
+        imageUrl: 'https://i.pinimg.com/564x/b6/4f/79/b64f798bbe07418db6abd047f49c3fa0.jpg',
+      },
+      {
+        id: 3,
+        name: 'rasya',
+        tag: 'raysa',
+        imageUrl: 'https://i.pinimg.com/564x/ed/79/b8/ed79b88276139af8a7bdd9b3c82d8a01.jpg',
+      },
+    ];
+   }
+    
+   export { getData };
